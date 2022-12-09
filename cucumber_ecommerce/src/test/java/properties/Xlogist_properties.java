9e1471343fdf110c29483c781924fe82953c6b44
abhimulay242 <120186382+abhimulay242@users.noreply.github.com>
2022-12-09 18:44:43 +0530

    Delete cucumber_ecommerce directory

--- cucumber_ecommerce/src/test/java/properties/Xlogist_properties.java
+++
@@ -1,7 +0,0 @@
-package properties;
-
-public class Xlogist_properties {
-	public final static String Ecommmerce = System.getProperty("user.dir")+"\\Features\\xlogist.feature";
-
-
-}
--- cucumber_ecommerce/src/test/java/properties/ecom_properties.java
+++
@@ -1,6 +0,0 @@
-package properties;
-
-public class ecom_properties {
-	public final static String Ecommmerce = System.getProperty("user.dir")+"\\Features\\ecom.feature";
-
-}
